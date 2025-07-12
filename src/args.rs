9ec88f060e347d16f3d4a7eb876af1ad4cc76819
root <localhost>
2025-07-12 23:07:02 +0800

    init

---
+++ src/args.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about)]
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Parser, Debug, Clone)]
+pub enum Commands {
+    Fs(crate::mods::fs::Param),
+    Loop(crate::mods::looper::Param),
+    Limit(limit::args::Args),
+    RBS(crate::mods::rbs::Param),
+    RBC(crate::mods::rbc::Param),
+    Install(crate::mods::install::Param),
+}
---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use clap::Parser;
+
+mod args;
+mod mods;
+mod utils;
+
+#[tokio::main]
+async fn main() -> tokio::io::Result<()> {
+    let args: args::Args = args::Args::parse();
+    match args.command {
+        args::Commands::Fs(param) => mods::fs::main(param).await,
+        args::Commands::Loop(param) => mods::looper::main(param).await,
+        args::Commands::Limit(param) => limit::main(param),
+        args::Commands::RBS(param) => mods::rbs::main(param).await,
+        args::Commands::RBC(param) => mods::rbc::main(param).await,
+        args::Commands::Install(param) => mods::install::main(param).await,
+    }
+}
